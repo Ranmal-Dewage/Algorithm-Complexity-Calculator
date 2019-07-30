1e3bd1bdd8e12a7ba05abfd1b26ae603715d99fd
Tenusha <tenusha_g@epiclanka.net>
2019-07-30 18:03:34 +0530

    with ctc

--- ACCScanner/src/main/java/com/sliit/spm/acc/FileHandler.java
+++ ACCScanner/src/main/java/com/sliit/spm/acc/FileHandler.java
@@ -92,6 +92,7 @@ public class FileHandler {
                     if (!singleLineCommented && !multiLineCommented) {
                         Cs.calcCs(lineObj, line, methodsAndVariables);
                         Ci.calcCi(lineObj, line);
+                        Ctc.calcCtc(lineObj, line);
                     }
 
                     if (line.trim().endsWith("*/")) {
@@ -107,6 +108,7 @@ public class FileHandler {
 
                 projectFiles.add(projectFile);
                 Ci.resetCi();
+                Ctc.setSwitchCtc();
 
             } catch (IOException e) {
                 LOGGER.error("Error reading file", e);
