9a2e56bc588374a4acf99f9bac9f172b08a63973
Tenusha <tenusha_g@epiclanka.net>
2019-07-30 09:06:26 +0530

    cpp support for Ci calculation

--- ACCScanner/src/main/java/com/sliit/spm/acc/Ci.java
+++ ACCScanner/src/main/java/com/sliit/spm/acc/Ci.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.sliit.spm.acc;
 
@@ -16,27 +16,27 @@ import com.sliit.spm.model.Line;
  */
 public class Ci {
 
-	private static final List<String> keywords = Arrays.asList("extends ", "implements ", ",");
-	private static int ci = 0;
+    private static final List<String> keywords = Arrays.asList("extends ", "implements ", ",", ":");
+    private static int ci = 0;
 
-	public static void calcCi(Line lineObj, String line) {
-		line = line.trim();
-		/*
-		 * find if line contains extends or implements keywords
-		 */
-		if (!line.isEmpty() && line.contains("class ")) {
-			ci = 2;
-		}
-		for (String keyword : keywords) {
-			if (!line.isEmpty() && line.indexOf(keyword) != -1 && line.contains("class ")) {
-				ci += StringUtils.countMatches(line, keyword);
-			}
-		}
-		lineObj.setCi((ci != 0 && !line.isEmpty() && !line.contains("class ")) ? ci : 0);
-	}
+    public static void calcCi(Line lineObj, String line) {
+        line = line.trim();
+        /*
+         * find if line contains extends or implements keywords
+         */
+        if (!line.isEmpty() && line.contains("class ")) {
+            ci = 2;
+        }
+        for (String keyword : keywords) {
+            if (!line.isEmpty() && line.indexOf(keyword) != -1 && line.contains("class ")) {
+                ci += StringUtils.countMatches(line, keyword);
+            }
+        }
+        lineObj.setCi((ci != 0 && !line.isEmpty() && !line.contains("class ")) ? ci : 0);
+    }
 
-	public static void resetCi() {
-		Ci.ci = 0;
-	}
+    public static void resetCi() {
+        Ci.ci = 0;
+    }
 
 }
