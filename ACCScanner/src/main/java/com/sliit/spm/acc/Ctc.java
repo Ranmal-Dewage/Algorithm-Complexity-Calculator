2ea1e55dcf84bae9acce7db6df7d27b84b0b407d
vimukthi_r <vimukthi.rajapaksha@my.sliit.lk>
2019-08-13 00:00:16 +0530

    with ctc unit tests

--- ACCScanner/src/main/java/com/sliit/spm/acc/Ctc.java
+++ ACCScanner/src/main/java/com/sliit/spm/acc/Ctc.java
@@ -3,12 +3,13 @@
  */
 package com.sliit.spm.acc;
 
-import com.sliit.spm.model.Line;
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.sliit.spm.model.Line;
 
 /**
  * @author Vimukthi Rajapaksha
@@ -16,45 +17,49 @@ import java.util.Objects;
  */
 public class Ctc {
 
-    private static final List<String> ccsKeywords = Arrays.asList("if(", "catch(", "if (", "catch (");
-    private static final List<String> icsKeywords = Arrays.asList("for(", "while(", "for (", "while (");
-    private static final List<String> bitwiseKeywords = Arrays.asList(" && ", " || ", " & ", " | ");
-    static Line switchLine = null;
-    static int switchCtc = 0;
+	private static final List<String> ccsKeywords = Arrays.asList("if(", "catch(", "if (", "catch (");
+	private static final List<String> icsKeywords = Arrays.asList("for(", "while(", "for (", "while (");
+	private static final List<String> bitwiseKeywords = Arrays.asList(" && ", " || ", " & ", " | ");
+	static Line switchLine = null;
+	static int switchCtc = 0;
 
-    public static void calcCtc(Line lineObj, String line) {
-        int ctc = 0;
-        line = line.trim();
+	public static void calcCtc(Line lineObj, String line) {
+		lineObj.setCtc(generateCtc(lineObj, line));
+	}
 
-        for (String keyword : ccsKeywords) {
-            if (!line.isEmpty() && line.indexOf(keyword) != -1) {
-                ctc += StringUtils.countMatches(line, keyword);
-                for (String bw : bitwiseKeywords) {
-                    ctc += StringUtils.countMatches(line, bw);
-                }
-            }
-        }
-        for (String keyword : icsKeywords) {
-            if (!line.isEmpty() && line.indexOf(keyword) != -1) {
-                ctc += StringUtils.countMatches(line, keyword) * 2;
-                for (String bw : bitwiseKeywords) {
-                    ctc += StringUtils.countMatches(line, bw) * 2;
-                }
-            }
-        }
-        if (!line.isEmpty() && line.contains("switch")) {
-            setSwitchCtc();
-            switchLine = lineObj;
-        }
-        switchCtc += StringUtils.countMatches(line, "case");
-        lineObj.setCtc(ctc);
-    }
+	public static int generateCtc(Line lineObj, String line) {
+		int ctc = 0;
+		line = line.trim();
 
-    public static void setSwitchCtc() {
-        if(Objects.nonNull(switchLine) && switchCtc > 0) {
-            switchLine.setCtc(switchCtc);
-        }
-        switchLine = null;
-        switchCtc = 0;
-    }
+		for (String keyword : ccsKeywords) {
+			if (!line.isEmpty() && line.indexOf(keyword) != -1) {
+				ctc += StringUtils.countMatches(line, keyword);
+				for (String bw : bitwiseKeywords) {
+					ctc += StringUtils.countMatches(line, bw);
+				}
+			}
+		}
+		for (String keyword : icsKeywords) {
+			if (!line.isEmpty() && line.indexOf(keyword) != -1) {
+				ctc += StringUtils.countMatches(line, keyword) * 2;
+				for (String bw : bitwiseKeywords) {
+					ctc += StringUtils.countMatches(line, bw) * 2;
+				}
+			}
+		}
+		if (!line.isEmpty() && line.contains("switch")) {
+			setSwitchCtc();
+			switchLine = lineObj;
+		}
+		switchCtc += StringUtils.countMatches(line, "case");
+		return ctc;
+	}
+
+	public static void setSwitchCtc() {
+		if (Objects.nonNull(switchLine) && switchCtc > 0) {
+			switchLine.setCtc(switchCtc);
+		}
+		switchLine = null;
+		switchCtc = 0;
+	}
 }
--- ACCScanner/src/main/java/com/sliit/spm/utils/Client.java
+++ ACCScanner/src/main/java/com/sliit/spm/utils/Client.java
@@ -1,53 +1,53 @@
 package com.sliit.spm.utils;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.sliit.spm.model.Project;
+import java.io.IOException;
+
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sliit.spm.model.Project;
 
 public class Client {
 
-    private static final Logger LOGGER = Logger.getLogger(Client.class);
-    public static final String FAILED_TO_SUBMIT_DATA_TO_THE_SERVER = "Failed to submit data to the server ";
-    private static String accUrl = PropertyReader.getInstance().getProperty("accUrl");
-    private static HttpClient httpClient = HttpClientBuilder.create().build();
-    private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+	private static final Logger LOGGER = Logger.getLogger(Client.class);
+	public static final String FAILED_TO_SUBMIT_DATA_TO_THE_SERVER = "Failed to submit data to the server ";
+	private static String accUrl = PropertyReader.getInstance().getProperty("accUrl");
+	private static HttpClient httpClient = HttpClientBuilder.create().build();
+	private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
-    private Client() {
-    }
+	private Client() {
+	}
 
-    public static void sendAnalysisData(Project project) {
-        try {
+	public static void sendAnalysisData(Project project) {
+		try {
 
-            LOGGER.debug("Submitting analysis data to the server...");
+			LOGGER.debug("Submitting analysis data to the server...");
 
-            HttpPost request = new HttpPost(accUrl);
-            request.addHeader("content-type", "application/json");
+			HttpPost request = new HttpPost(accUrl);
+			request.addHeader("content-type", "application/json");
 
-            String json = gson.toJson(project); //convert
-            LOGGER.info(json);
+			String json = gson.toJson(project); // convert
+			LOGGER.info(json);
 
-            StringEntity params = new StringEntity(json);
-            request.setEntity(params);
+			StringEntity params = new StringEntity(json);
+			request.setEntity(params);
 
-            HttpResponse response = httpClient.execute(request);
+			HttpResponse response = httpClient.execute(request);
 
-            if (response.getStatusLine().getStatusCode() == 200) {
-                LOGGER.info("Analysis data submitted to the server " + response.getStatusLine());
-            } else {
-                LOGGER.error(FAILED_TO_SUBMIT_DATA_TO_THE_SERVER + response.getStatusLine());
-            }
+			if (response.getStatusLine().getStatusCode() == 200) {
+				LOGGER.info("Analysis data submitted to the server " + response.getStatusLine());
+			} else {
+				LOGGER.error(FAILED_TO_SUBMIT_DATA_TO_THE_SERVER + response.getStatusLine());
+			}
 
-        } catch (IOException e) {
-            LOGGER.error(FAILED_TO_SUBMIT_DATA_TO_THE_SERVER);
-        }
-    }
+		} catch (IOException e) {
+			LOGGER.error(FAILED_TO_SUBMIT_DATA_TO_THE_SERVER);
+		}
+	}
 }
